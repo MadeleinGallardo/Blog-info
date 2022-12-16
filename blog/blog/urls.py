60b51dc71b7419e27e5feb959ffdd002afc439c9
MadeleinAbrilGallardo <madeleinagallardo@gmail.com>
2022-12-16 19:18:05 -0300

    Imagen de post

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, re_path
 from apps.post.views import *
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -23,4 +25,4 @@ urlpatterns = [
     path('posteos/',(MostrarPost.as_view()), name='posteos'),
     re_path('leer_post/(?P<id>\d+)/$', leerPost, name='posteo'),
 
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
